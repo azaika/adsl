2e2d2a2066ed638921dd30894f8627efddb9eda2
azaika <takaaza@gmail.com>
2019-10-26 03:01:35 +0900

    Make run_verify.sh more useful

--- verify/run_verify.sh
+++ verify/run_verify.sh
@@ -2,6 +2,12 @@
 
 sh_dir=`\dirname $0`
 
+run_times=$2
+if [ "" = "$run_times" ]
+then
+	run_times=5
+fi
+
 ~/usr/bin/g++ -Wall -Wextra -std=c++2a -O2 -I $sh_dir/../include $sh_dir/$1/source.cpp
 
 for file in `\find $sh_dir/$1/in -maxdepth 1 -type f | sed -E 's!^.*/(.+)\.in!\1!' | sort -f`; do
@@ -9,7 +15,7 @@ for file in `\find $sh_dir/$1/in -maxdepth 1 -type f | sed -E 's!^.*/(.+)\.in!\1
 	./a.out < "$sh_dir/$1/in/$file.in" > tmp_output
 
 	ave_time=0
-	for i in `seq 5`; do
+	for i in `seq $run_times`; do
 		start_time=$(date +"%s%3N")
 
 		./a.out < "$sh_dir/$1/in/$file.in" > tmp_output
@@ -19,7 +25,7 @@ for file in `\find $sh_dir/$1/in -maxdepth 1 -type f | sed -E 's!^.*/(.+)\.in!\1
 		ave_time=$((ave_time + end_time - start_time))
 	done
 	
-	ave_time=$((ave_time/5))
+	ave_time=$((ave_time/$run_times))
 
 	if [ "" = "`diff -q tmp_output $sh_dir/$1/out/$file.out`" ]
 	then
